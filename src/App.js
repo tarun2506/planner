a249906c54e4162eb6cc0efddbbfbacba2318800
tarun2506 <tarunsharma8920@gmail.com>
2021-09-17 16:20:02 +0530

    dashboard component & notifications & project list done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function ProjectList() {
+  return (
+    <div className="section project-list">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by xO</p>
+          <p className="grey-text">2nd October, 4pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by xO</p>
+          <p className="grey-text">2nd October, 4pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by xO</p>
+          <p className="grey-text">2nd October, 4pm</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by xO</p>
+          <p className="grey-text">2nd October, 4pm</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectList;
