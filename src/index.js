706ca0f2b7ee4a6a328e3e28fc99bd4b4e5f5cfe
tarun2506 <tarunsharma8920@gmail.com>
2021-09-20 17:41:24 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App";
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import thunk from "redux-thunk";
 import {
   createFirestoreInstance,
   getFirestore,
   reduxFirestore,
 } from "redux-firestore";
-import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase,
+  isLoaded,
+} from "react-redux-firebase";
 import fbConfig from "./config/FirebaseConfig";
 import firebase from "firebase/app";
 
@@ -30,10 +34,18 @@ const rrfProps = {
   createFirestoreInstance,
 };
 
+function AuthIsLoaded({ children }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+  return children;
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
